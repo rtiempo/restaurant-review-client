64ca6e97e46299b73c129c6c13055811f867feb1
Robert Tiempo <jroberttiempo58@gmail.com>
2022-02-26 21:25:18 +0800

    Add initial components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar, Main } from './components';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Main />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Login } from './Login/Login';
+export { default as Main } from './Main/Main';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Restaurant } from './Restaurant/Restaurant';
+export { default as RestaurantList } from './Restaurant/RestaurantList';
+export { default as NewReview } from './Review/NewReview';
+export { default as Review } from './Review/Review';
