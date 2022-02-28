45062cd275decf9a6b067554ac1dd6a14082b87b
Robert Tiempo <jroberttiempo58@gmail.com>
2022-03-01 04:02:23 +0800

    Fixtest netlify

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL:
+    process.env.REACT_APP_BASE_URL ||
+    'https://restaurant-review-server.herokuapp.com/api/v1',
   headers: {
     'Content-type': 'application/json',
   },
