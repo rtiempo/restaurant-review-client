8a8a5199c7f7ddf51274b536e30fe3142c566969
Robert Tiempo <jroberttiempo58@gmail.com>
2022-02-28 22:58:38 +0800

    Refactor hard coded base url to env var

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     'Content-type': 'application/json',
   },
